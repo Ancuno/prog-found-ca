4437177384deb27daae2767b632b34ab91507344
Ancuno <56104063+Ancuno@users.noreply.github.com>
2020-01-21 20:58:11 +0100

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,39 @@
+//Question 1
+
+var animal = "dog";
+
+//Question 2
+
+var person = {
+    name: "Vlad",
+    age: 31
+};
+
+//Question 3
+
+outOfStock = true; 
+
+if(outOfStock === true) {
+    console.log("Out of stock");
+} 
+else {
+    console.log("In stock");
+}
+
+//Question 4
+
+var numbers = [11,22,33,44,55];
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+          
+//Question 5
+
+for(var i = 15; i <= 25; i++) {
+    console.log(i);
+}
+
+//Question 6
